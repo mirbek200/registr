2b86478f0465ba1f15c4beda2b7e3a57399a510a
uyfguiygiuy <you@example.com>
2022-08-09 18:00:55 -1200

    features2 second commit

--- chapter2/test1.py
+++ chapter2/test1.py
@@ -1,6 +1,40 @@
 import re
 
-pattern = r' '
-nam = ''
-result = re.sub(pattern, nam, "Python Exercises")
-print(result)
+# p = re.compile(r'\d+.\d+.\d+.\d+.-')
+
+# p = re.compile(r'\d+\w+\d+:\d+:\d+:\d+ \+\d+')
+
+# p = re.compile(r'GET')
+
+p = re.compile(r'\d+.\d+.\d+.\d+.-|\d+\w+\d+:\d+:\d+:\d+ \+\d+|GET')
+
+text = '''109.169.248.247 - - [12/Dec/2015:18:25:11 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+109.169.248.247 - - [12/Dec/2015:18:25:11 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+46.72.177.4 - - [12/Dec/2015:18:31:08 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+46.72.177.4 - - [12/Dec/2015:18:31:08 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+83.167.113.100 - - [12/Dec/2015:18:31:25 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+83.167.113.100 - - [12/Dec/2015:18:31:25 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+95.29.198.15 - - [12/Dec/2015:18:32:10 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+95.29.198.15 - - [12/Dec/2015:18:32:11 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+109.184.11.34 - - [12/Dec/2015:18:32:56 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+109.184.11.34 - - [12/Dec/2015:18:32:56 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+91.227.29.79 - - [12/Dec/2015:18:33:51 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+91.227.29.79 - - [12/Dec/2015:18:33:52 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+90.154.66.233 - - [12/Dec/2015:18:36:16 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+90.154.66.233 - - [12/Dec/2015:18:36:16 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+95.140.24.131 - - [12/Dec/2015:18:38:42 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+95.140.24.131 - - [12/Dec/2015:18:38:42 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+95.188.245.16 - - [12/Dec/2015:18:38:55 +0100] "GET /administrator/ HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+95.188.245.16 - - [12/Dec/2015:18:38:56 +0100] "POST /administrator/index.php HTTP/1.1" 200 4494 "http://almhuette-raith.at/administrator/" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"
+'''
+a = p.findall(text)
+
+for i in a:
+    if i == 'GET':
+        index = int(a.index('GET'))
+        index_date = index - 1
+        index_id = index - 2
+        print(f'{a[index_id]} {a[index_date]} {a[index]}')
+        a.remove('GET')
+    else:
+        continue
--- chapter2/test2.py
+++ chapter2/test2.py
@@ -1,42 +1,6 @@
 import re
 
-print('Давайте зарегистрируемся')
-password = input('Введите пароль: ')
-
-l, u, n, znack, trash = 0, 0, 0, 0,0
-
-pattern_u = r"[A-Z]"
-res_u = re.findall(pattern_u, password)
-
-pattern_n = r"[0-9]"
-res_n =  re.findall(pattern_n, password)
-
-pattern_l = r"[a-z]"
-res_l =  re.findall(pattern_l, password)
-
-for i in password:
-    if i in res_u:
-        u+=1
-    elif i in res_n:
-        n+=1
-    elif i in res_l:
-        l+=1
-    elif i in '$%!@#^&*':
-        znack+=1
-    else:
-        trash+=1
-
-if len(password) < 8:
-    print('Пароль должен быть длинее восьми символов')
-elif n == 0:
-    print('Пароль должен содержать цифры')
-elif u == 0:
-    print('Пароль должен содержать заглавные буквы')
-elif l == 0:
-    print('Пароль должен содержать маленькие буквы')
-elif znack == 0:
-    print('Пароль должен содержать специальные символы как $%!@#^&*')
-elif trash != 0:
-    print('ERROR')
-else:
-    print('Вы успешно зарегистрированы')
+pattern = r' '
+nam = ''
+result = re.sub(pattern, nam, "Python Exercises")
+print(result)
